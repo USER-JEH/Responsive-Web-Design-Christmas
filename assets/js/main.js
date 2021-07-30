bc83bc6f107de38ed325dd0720beb49b3c643d5d
USER-JEH <han880330@yahoo.co.jp>
2021-07-30 16:34:09 +0900

    menu

---
+++ assets/js/main.js
@@ -0,0 +1,73 @@
+/*==================== SHOW MENU ====================*/
+const showMenu = (toggleId, navId) =>{
+	const toggle = document.getElementById(toggleId),
+	nav = document.getElementById(navId)
+	
+	// Validate that variables exist
+	if(toggle && nav){
+			toggle.addEventListener('click', ()=>{
+					// We add the show-menu class to the div tag with the nav__menu class
+					nav.classList.toggle('show-menu')
+			})
+	}
+}
+showMenu('nav-toggle','nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+
+	// When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+
+
+
+/*==================== SHOW SCROLL TOP ====================*/ 
+
+	// When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
+
+
+// Previously selected topic (if user selected)
+
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+
+
+// We validate if the user previously chose a topic
+
+// If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+
+
+// Activate / deactivate the theme manually with the button
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
